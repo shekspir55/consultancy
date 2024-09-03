90bdad2057e51f6b136e693a176a97c4859af77c
Ruben Shekspir <shekspir55@gmail.com>
2024-09-03 22:14:45 +0400

    cal link changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -298,7 +298,7 @@ export default function Home() {
                   <div className="card-actions justify-end">
                     <a
                       className="btn btn-primary"
-                      href="https://www.linkedin.com/in/ruben-kh/"
+                      href="https://cal.com/ruben-pbl/"
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
