e3b4633fb90b1eaee09f58c87b20f74a04099422
Ruben Shekspir <shekspir55@gmail.com>
2024-09-19 21:45:46 +0400

    build fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -389,10 +389,10 @@ export default function Home() {
             height="315"
             src="https://www.youtube.com/embed/wOBOWZ7I-sM?si=QjOVjj-4DJUcv2ww"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
           ></iframe>
         </div>
       </div>
