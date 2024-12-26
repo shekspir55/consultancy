aad82df6f38c274eb21b6c4aa7210d304a66dbe9
Ruben Shekspir <shekspir55@gmail.com>
2024-12-26 09:34:54 +0400

    no commernt

--- src/app/utils/podcast.ts
+++ src/app/utils/podcast.ts
@@ -69,25 +69,7 @@ export default async function generatePodcastRssFeed() {
       itunesNewFeedUrl: `${websiteUrl}${urlPathToPodcasts}/rss.xml`,
     });
   });
-
-  // /* loop over data and add to feed */
-  // feed.addItem({
-  //     title: 'item title',
-  //     description: 'use this for the content. It can include html.',
-  //     url: 'http://example.com/article4?this&that', // link to the item
-  //     // guid: '1123', // optional - defaults to url
-  //     categories: ['Category 1', 'Category 2', 'Category 3', 'Category 4'], // optional - array of item categories
-  //     author: 'Guest Author', // optional - defaults to feed author property
-  //     date: 'May 27, 2012', // any format that js Date can parse.
-
-  //     itunesAuthor: 'Max Nowack',
-  //     itunesExplicit: false,
-  //     itunesSubtitle: 'I am a sub title',
-  //     itunesSummary: 'I am a summary',
-  //     itunesDuration: 12345,
-  //     itunesNewFeedUrl: 'https://newlocation.com/example.rss',
-  // });
-
+  
   const xml = feed.buildXml();
 
   fs.writeFileSync(pathToFile, xml);
